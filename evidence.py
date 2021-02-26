46ede09f490c1bded2a3a3be5268f7882b91150a
AƧTЯOƆAT <jinglinzhao@gmail.com>
2021-02-26 17:20:21 -0500

    added Maximum Likelihood Estimation

--- evidence.py
+++ evidence.py
@@ -57,3 +57,43 @@ plt.xlabel('number of activity indicators')
 plt.ylabel('final evidence')
 plt.savefig('final_evidence.png')
 plt.show()
+
+#------------------------------
+# Maximum Likelihood Estimation
+#------------------------------
+N = 100
+model_mean = np.linspace(-2, 2, N)
+logP = np.zeros((6,N))
+
+# Approach 1 - Chi-squared
+for i in range(6):
+	for k in range(N):
+		C = np.loadtxt(file_dir + '101501_C' + str(i+1) + '.txt')
+		C_err = np.loadtxt(file_dir + '101501_err_C' + str(i+1) + '.txt')
+		X = sum(((C-model_mean[k])/C_err)**2)
+		logP[i, k] = np.log(chi2.pdf(X, len(C)))
+
+	plt.plot(model_mean, logP[i, :])
+	plt.title('chi2_logP '+str(i))
+	plt.xlabel('constant_model')
+	plt.ylabel('log-likelihood')
+	plt.axvline(x=np.average(C, weights=1/C_err**2))
+	plt.savefig('chi2_logP'+str(i)+'.png')
+	plt.close()
+# The plots above show that the mean estimated by
+# maximising the chi-squared log-likelihood function
+# is consistent with the weighted mean of the data.
+
+# Approach 2 - Gaussian
+for i in range(6):
+	for k in range(N):
+		C = np.loadtxt(file_dir + '101501_C' + str(i+1) + '.txt')
+		C_err = np.loadtxt(file_dir + '101501_err_C' + str(i+1) + '.txt')
+		logP[i, k] = sum(np.log(norm.pdf(model_mean[k], C, C_err)))
+	plt.plot(model_mean, logP[i, :])
+	plt.title('gaussian_logP ' + str(i))
+	plt.xlabel('constant_model')
+	plt.ylabel('log-likelihood')
+	plt.axvline(x=np.average(C, weights=1/C_err**2))
+	plt.savefig('gaussian_logP'+str(i)+'.png')
+	plt.close()
